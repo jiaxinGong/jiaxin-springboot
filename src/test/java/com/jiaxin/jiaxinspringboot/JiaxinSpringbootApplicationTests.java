593951dea4add897de26f92ac30d3ff2046e86e7
zhangcs <1223895481@qq.com>
2018-04-28 17:23:36 +0800

    Initial commit

---
+++ src/test/java/com/jiaxin/jiaxinspringboot/JiaxinSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jiaxin.jiaxinspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiaxinSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
